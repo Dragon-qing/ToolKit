9cb409779c42ace479cf301b004811c930a3e2d7
Dragon-qing <1259337898@qq.com>
2026-04-22 16:07:23 +0800

    chore: 修改界面初始大小

--- src/utils/common.h
+++ src/utils/common.h
@@ -14,11 +14,11 @@
 #define UTF_8 "UTF-8"
 #define FONT_STYLE "Microsoft YaHei"
 
-const Bit32 WINDOW_WIDTH =  1024;
-const Bit32 WINDOW_HEIGHT = 768;
+constexpr Bit32 WINDOW_WIDTH =  1280;
+constexpr Bit32 WINDOW_HEIGHT = 800;
 
-const Bit32 DLG_WIDTH =  400;   // 对话框宽度
-const Bit32 DLG_HEIGHT = 300;   // 对话框高度
+constexpr Bit32 DLG_WIDTH =  400;   // 对话框宽度
+constexpr Bit32 DLG_HEIGHT = 300;   // 对话框高度
 
 #ifdef QT_DEBUG // 测试环境
 // 输出到控制台
@@ -34,7 +34,7 @@ void ComDebug(const QStringList content, const QString &title = "");
 // 状态栏提示
 void InitStatusBar(QStatusBar *statusBar); // 初始化状态栏
 void DestoryStatusBar(); // 销毁状态栏
-void PromptOut(const QString &content, Bit32 outtime = 0);
+void PromptOut(const QString &content, Bit32 outtime = 3000);
 
 // 获取数据的范围
 QPair<fBit64, fBit64> GetRange(const QVector<fBit64> &in);
