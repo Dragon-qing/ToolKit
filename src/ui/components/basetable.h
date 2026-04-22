04a325edbd649b5dc7496cb890de919f145b28c6
Dragon-qing <1259337898@qq.com>
2026-04-22 23:37:37 +0800

    func: basetable控件支持tips和行拖拽功能

--- src/ui/components/basetable.h
+++ src/ui/components/basetable.h
@@ -1,8 +1,10 @@
 ﻿#ifndef BASETABLE_H
 #define BASETABLE_H
 
+#include <qobjectdefs.h>
 #include <QWidget>
 #include <QTableView>
+#include <QStyledItemDelegate>
 
 #include "datadef.h"
 
@@ -33,8 +35,15 @@ public:
 
     void Refresh(); // 仅更新已有数据
     void ReDraw(); // 重新构建数据
+
 protected:
     TKTableModel *m_pModel;
+    /**
+     * @brief: 初始化表格
+     * @param {QStringList} headList: 表头列表
+     * @param {QVector<Bit32>} scale: 列宽比例
+     * @return {Bit32} 0:成功，其他:失败
+     */
     Bit32 InitTable(QStringList headList, QVector<Bit32> scale);
 
     void resizeEvent(QResizeEvent *event) override;
@@ -59,12 +68,29 @@ public:
     void InsertData(QStringList list); // 尾插数据
     void InsertHead(QString head); // 尾插标题头
     void ClearData(); // 清空数据
+
     bool removeRows(int row, int count, const QModelIndex &parent) override;
+    Qt::ItemFlags flags(const QModelIndex &index) const override;
+    QStringList mimeTypes() const override;
+    bool dropMimeData(const QMimeData *data, Qt::DropAction action, int row, int column, const QModelIndex &parent) override;
+    Qt::DropActions supportedDropActions() const override;
+    QMimeData *mimeData(const QModelIndexList &indexes) const override;
+
+signals:
+    // 行移动信号
+    void RowMoved(int from, int to); 
 
 private:
     QVector<QStringList> m_dataVec; // 数据
     QStringList m_headList; // 标题头
+};
 
+class TooltipDelegate : public QStyledItemDelegate {
+public:
+    explicit TooltipDelegate(QObject *parent = nullptr) : QStyledItemDelegate(parent) {}
+    ~TooltipDelegate() override = default;
+    bool helpEvent(QHelpEvent *event, QAbstractItemView *view,
+                   const QStyleOptionViewItem &option, const QModelIndex &index) override;
 };
 
 #endif // BASETABLE_H
